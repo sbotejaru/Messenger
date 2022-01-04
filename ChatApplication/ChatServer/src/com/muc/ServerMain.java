4f6b98ccbf40e11f7912c787bb6b46f339bcf0d9
sbotejaru <39643893+sbotejaru@users.noreply.github.com>
2022-01-04 20:01:17 +0200

    created repo

---
+++ ChatApplication/ChatServer/src/com/muc/ServerMain.java
@@ -0,0 +1,12 @@
+package com.muc;
+
+/**
+ * Created by jim on 4/18/17.
+ */
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        Server server = new Server(port);
+        server.start();
+    }
+}
